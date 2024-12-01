39d4ee0267fea76627c5fbfd0e0e348e5941efd7
Hugobsan <hugocorone10@gmail.com>
2024-12-01 04:05:10 -0300

    Tornado a home dinâmica e recarregável

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rain_alert/modules/home/home_page_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<void> _refreshData;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData = _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final homeController = Provider.of<HomeController>(context, listen: false);
+    await homeController.loadSettings();
+    await homeController.loadWeatherData();
+    await homeController.loadForecastData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,22 +70,11 @@ class HomePage extends StatelessWidget {
                 ? const Color.fromARGB(255, 2, 115, 209)
                 : const Color.fromARGB(255, 53, 67, 102),
             onSelected: (value) {
-              if (value == 'login') {
-                Navigator.of(context).pushNamed('/login');
-              } else if (value == 'settings') {
+              if (value == 'settings') {
                 Navigator.of(context).pushNamed('/settings');
-              } else if (value == 'notifications') {
-                Navigator.of(context).pushNamed('/notifications');
               }
             },
             itemBuilder: (BuildContext context) => [
-              PopupMenuItem(
-                value: 'login',
-                child: Text(
-                  'Login',
-                  style: TextStyle(color: textColor),
-                ),
-              ),
               PopupMenuItem(
                 value: 'settings',
                 child: Text(
@@ -73,98 +82,120 @@ class HomePage extends StatelessWidget {
                   style: TextStyle(color: textColor),
                 ),
               ),
-              PopupMenuItem(
-                value: 'notifications',
-                child: Text(
-                  'Histórico de Notificações',
-                  style: TextStyle(color: textColor),
-                ),
-              ),
             ],
           ),
         ],
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: homeController.getBackgroundGradient(),
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: FutureBuilder<Map<String, dynamic>?>(
-                future: homeController.loadWeatherData(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError || snapshot.data == null) {
-                    return Center(
-                      child: Text(
-                        "Erro ao carregar dados do clima.",
-                        style: TextStyle(color: textColor),
-                      ),
-                    );
-                  } else {
-                    final weatherData = snapshot.data!;
-                    final description = homeController.formatWeatherDescription(
-                        weatherData['weather'][0]['description']);
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "${weatherData['main']['temp'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'}",
-                            style: TextStyle(fontSize: 75, color: textColor),
-                          ),
-                          Icon(
-                            homeController.getWeatherIcon(description),
-                            size: 80,
-                            color: textColor,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            description,
-                            style: TextStyle(fontSize: 18, color: textColor),
-                          ),
-                          Text(
-                            "Mínima: ${weatherData['main']['temp_min'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'} / Máxima: ${weatherData['main']['temp_max'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'}",
-                            style: TextStyle(fontSize: 16, color: textColor),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                },
-              ),
-            ),
-            const SizedBox(height: 75),
-            Expanded(
-              flex: 3,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 16.0),
-                    child: FutureBuilder<void>(
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {
+            _refreshData = _loadData();
+          });
+          await _refreshData;
+        },
+        child: FutureBuilder<void>(
+          future: _refreshData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  "Erro ao carregar os dados.",
+                  style: TextStyle(color: textColor),
+                ),
+              );
+            }
+            return SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: homeController.getBackgroundGradient(),
+                ),
+                constraints: BoxConstraints(
+                  minHeight: MediaQuery.of(context).size.height,
+                ),
+                child: Column(
+                  children: [
+                    // Dados do clima atual
+                    FutureBuilder<Map<String, dynamic>?>(
+                      future: homeController.loadWeatherData(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Padding(
+                            padding: EdgeInsets.only(top: 100),
+                            child: Center(child: CircularProgressIndicator()),
+                          );
+                        } else if (snapshot.hasError || snapshot.data == null) {
+                          return Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text(
+                                "Erro ao carregar dados do clima.",
+                                style: TextStyle(color: textColor),
+                              ),
+                            ),
+                          );
+                        } else {
+                          final weatherData = snapshot.data!;
+                          final description = homeController
+                              .formatWeatherDescription(
+                                  weatherData['weather'][0]['description']);
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "${weatherData['main']['temp'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'}",
+                                  style:
+                                      TextStyle(fontSize: 75, color: textColor),
+                                ),
+                                Icon(
+                                  homeController.getWeatherIcon(description),
+                                  size: 80,
+                                  color: textColor,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  description,
+                                  style:
+                                      TextStyle(fontSize: 18, color: textColor),
+                                ),
+                                Text(
+                                  "Mínima: ${weatherData['main']['temp_min'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'} / Máxima: ${weatherData['main']['temp_max'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'}",
+                                  style:
+                                      TextStyle(fontSize: 16, color: textColor),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      },
+                    ),
+
+                    // Previsão para os próximos dias
+                    FutureBuilder<void>(
                       future: homeController.loadSettings(),
                       builder: (context, snapshot) {
                         final count =
                             homeController.currentSettings?.predCitiesCount ??
                                 6;
-                        return Text(
-                          "Previsão para os próximos $count dias",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: textColor,
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          child: Text(
+                            "Previsão para os próximos $count dias",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: textColor,
+                            ),
                           ),
                         );
                       },
                     ),
-                  ),
-                  Expanded(
-                    child: FutureBuilder<List<Map<String, dynamic>>>(
+                    FutureBuilder<List<Map<String, dynamic>>>(
                       future: homeController.loadForecastData(),
                       builder: (context, snapshot) {
                         if (snapshot.connectionState ==
@@ -172,7 +203,8 @@ class HomePage extends StatelessWidget {
                           return const Center(
                             child: CircularProgressIndicator(),
                           );
-                        } else if (snapshot.hasError || snapshot.data == null) {
+                        } else if (snapshot.hasError ||
+                            snapshot.data == null) {
                           return Center(
                             child: Text(
                               "Erro ao carregar previsão.",
@@ -182,6 +214,8 @@ class HomePage extends StatelessWidget {
                         } else {
                           final forecastData = snapshot.data!;
                           return ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
                             itemCount: forecastData.length,
                             itemBuilder: (context, index) {
                               final dayForecast = forecastData[index];
@@ -197,8 +231,8 @@ class HomePage extends StatelessWidget {
                                 ),
                                 trailing: Text(
                                   "${dayForecast['temp_max'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'} / ${dayForecast['temp_min'].toStringAsFixed(1)}${homeController.currentSettings?.getFormattedTempUnit()['unit'] ?? '°C'}",
-                                  style:
-                                      TextStyle(color: textColor, fontSize: 16),
+                                  style: TextStyle(
+                                      color: textColor, fontSize: 16),
                                 ),
                               );
                             },
@@ -206,11 +240,11 @@ class HomePage extends StatelessWidget {
                         }
                       },
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
